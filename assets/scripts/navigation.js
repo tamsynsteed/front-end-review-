275267904a783e478cd029bd9f8640665928de46
Tamsyn Steed <71071834+tamsynsteed@users.noreply.github.com>
2021-02-16 13:04:30 +0200

    Delete assets directory

--- assets/scripts/navigation.js
+++
@@ -1,9 +0,0 @@
-function toggleNav() {
-  // Get links from navigation
-  const links = document.getElementById("nav-links");
-
-  //   Check styles on links
-  links.style.width == "0px"
-    ? (links.style.width = "100vw")
-    : (links.style.width = "0px");
-}
